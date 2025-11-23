e1a3158c2ecbf79d506b9e3bd28c4dd7f8e22ea9
DarkBones <23454780+DarkBones@users.noreply.github.com>
2025-11-23 08:48:34 +0100

    Add actual config

--- zsh/.zsh/roles/bas.zsh
+++ zsh/.zsh/roles/bas.zsh
@@ -1,1 +1,11 @@
-echo "BAS LOADED"
+# Start tmuxifier session
+function s() {
+  [ -z $1 ] && {
+    echo "usage: s [tmuxifier-session-name]" >&2
+    echo "example:" >&2
+    echo "s dotfiles" >&2
+    return 1
+  }
+
+  tmuxifier s "$1"
+}
